d08e742959008b40086aa92ae63cc16a6ef9eba0
Orlando <programadorlando@gmail.com>
2022-12-29 12:20:45 -0300

    first commit: webpage structure

---
+++ _javascript/funcoes.js
@@ -0,0 +1,22 @@
+function loadFunction() {
+	const value = localStorage.getItem('background-color');
+	let container = document.getElementById('general');
+	container.style.backgroundColor = `#${value}`;
+	console.log(value);
+}
+
+function mudaFoto(foto) {
+	document.getElementById("icone").src = "_imagens/" + foto + ".png";
+};
+
+function backgroundChange() {
+	let container = document.getElementById('general');
+	container.style.backgroundColor = '#424242';
+	localStorage.setItem('background-color', '424242');
+}
+
+function backgroundChangeLight() {
+	let container = document.getElementById('general');
+	container.style.backgroundColor = '#ffffff';
+	localStorage.setItem('background-color', 'ffffff');
+}
